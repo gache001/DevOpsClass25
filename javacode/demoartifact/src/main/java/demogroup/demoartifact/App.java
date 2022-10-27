00aeefa87fd5e1c60077285e14ddac557891a8c1
gache001 <114547199+gache001@users.noreply.github.com>
2022-10-27 04:22:57 -0400

    Delete javacode directory

--- javacode/demoartifact/src/main/java/demogroup/demoartifact/App.java
+++
@@ -1,8 +0,0 @@
-package demogroup.demoartifact;
-
-public class App {
-
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
-    }
-}
--- javacode/ronak_test/src/module-info.java
+++
@@ -1,2 +0,0 @@
-module ronak_test {
-}
